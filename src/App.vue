5dc1cd86d0f89ff4924922cf3795940dd6db52d1
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-13 15:45:02 +0500

    отключаем обновление мтраници при нажатии на кнопку

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,23 @@
     <h1>Hello</h1>
     <keep-alive>
       <component :is="changeComponent"></component>
-
     </keep-alive>
     
-    <hr />
     <button @click="nameComponent = 'One'" class="btn btn-success">ONE</button>
     <hr />
     <button @click="nameComponent = 'Two'" class="btn btn-success">TWO</button>
     <hr />
-    <button @click="nameComponent = 'Three'" class="btn btn-success">THREE</button>
+    <button @click="nameComponent = 'Three'" class="btn btn-success">
+      THREE
+    </button>
     <hr />
+    <FormsBlock /> 
+     <form @submit="formSubmit">
+      <input type="text" />
+      <br/>
+      <hr/>
+      <button type="submit" class="btn btn-primary">Отправить</button>
+    </form>
   </div>
 </template>
 
@@ -20,6 +27,7 @@
 import BlockContentOne from "./BlockContentOne.vue";
 import BlockContentTwo from "./BlockContentTwo.vue";
 import BlockContentThree from "./BlockContentThree.vue";
+import FormsBlock from "./FormsBlock.vue";
 
 export default {
   data() {
@@ -29,14 +37,19 @@ export default {
   },
   computed: {
     changeComponent() {
-      return 'BlockContent' + this.nameComponent
-    }
-
+      return "BlockContent" + this.nameComponent;
+    },
   },
+   methods: {
+     formSubmit(e) {
+       e.preventDefault()
+     }
+   },
   components: {
     BlockContentOne,
     BlockContentTwo,
     BlockContentThree,
+    FormsBlock,
   },
 };
 </script>
