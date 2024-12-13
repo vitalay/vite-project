1b39ae3105f0d78b972f55c4e05906b56f23a868
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-13 16:04:34 +0500

    2 варианта таботы с формами через v-model лучше

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,12 @@
     </button>
     <hr />
     <FormsBlock /> 
-     <form @submit.prevent>
-      <input type="text" />
+     <form @submit.prevent="formSubmit">
+      <input type="text" :value="inputOne" @input="inputChangeOne" class="form-control">
+      <input type="text" v-model="inputTwo" class="form-control">
       <br/>
       <hr/>
-      <button type="submit" class="btn btn-primary">Отправить</button>
+      <button type="submit" :value="inputTwo" class="btn btn-primary">Отправить</button>
     </form>
   </div>
 </template>
@@ -32,6 +33,8 @@ import FormsBlock from "./FormsBlock.vue";
 export default {
   data() {
     return {
+      inputOne: "",
+      inputTwo: "",
       nameComponent: "One",
     };
   },
@@ -39,6 +42,18 @@ export default {
     changeComponent() {
       return "BlockContent" + this.nameComponent;
     },
+  },
+  methods: {
+    formSubmit() {
+      console.log('formSubmit', this.inputOne)
+      console.log('formSubmit', this.inputTwo)
+    },
+     
+    inputChangeOne(event) {
+      console.log('inputChangeOne', event.target.value)
+      this.inputOne = event.target.value
+    }
+ 
   },
  
   components: {
